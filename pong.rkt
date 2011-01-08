9e20891b0c619dc34a2bba19d9bbc406cc48e538
Mucahit Senol <bcursor@gmail.com>
2011-01-08 02:29:55 +0200

    Ball structure is defined

--- pong.rkt
+++ pong.rkt
@@ -1,5 +1,16 @@
-; Pong Project written by Bilgi students
-;  
-
 #lang racket 
+(require 2htdp/image)
+
+; Pong project written by Bilgi CS students
+
+
+
+(define-struct Ball (x y vx vy radius color mode))
+; - x is a Number, x coordinate of Ball
+; - y is a Number, y coordinate of Ball
+; - vx is a Number, velocity on x plane
+; - vy is a Number, velocity on y plane
+; - radius is a Number, radius of Ball
+; - color is a Color, color of Ball
+; - mode is a String, "outline" or "solid"
 
