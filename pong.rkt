871f94bbae377cea20beea9b1b6963f3c0c457bf
Mucahit Senol <bcursor@gmail.com>
2011-01-08 02:21:42 +0200

    second commit by smartgit

--- pong.rkt
+++ pong.rkt
@@ -1,5 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname pong) (read-case-sensitive #t) (teachpacks ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "htdp")))))
 ; Pong Project written by Bilgi students
-; 
+;  
+
+#lang racket 
+
